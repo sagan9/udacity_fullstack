8e353bb9f1e443ee7cf53aa4a8a11556940d3f34
sagan9 <guanming.kim@gmail.com>
2017-10-17 17:43:22 +0900

    Studied until classes.

--- lec1/lesson5/rename_files.py
+++ lec1/lesson5/rename_files.py
@@ -1,3 +1,14 @@
 import os
 
-print(os.listdir("./prank"))
+def rename_files():
+    file_list = os.listdir("./prank")
+    # print(file_list)
+    saved_path = os.getcwd()
+    print(saved_path)
+    os.chdir("./prank")
+    
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+        print(file_name)
+
+rename_files()
